ee58b198af2d071786cdd0de441b130cec4c2850
David Allan <profissional@david-allan.com.br>
2016-12-27 15:18:22 -0200

    Created Security key class to use in webservice

---
+++ webservice/api/common/security/key/key.php
@@ -0,0 +1,20 @@
+<?
+// By David Allan
+class key
+{
+    private $_year;
+    private $_key;
+
+    /**
+     * key constructor.
+     */
+    function __construct(){
+        $_key = "3r323r$%¨&@#$%¨&*(ueivbe4b834484345247237754y73231'242r7&*¨%ERT&¨%";
+    }
+
+    public GetKey()
+    {
+        return $_key;
+    }
+}
+?>
